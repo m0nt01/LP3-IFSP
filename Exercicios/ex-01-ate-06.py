5375597901d748a5f22f09cdeb76fc238f64d10c
Guilherme Henrique Monteiro <107049790+m0nt01@users.noreply.github.com>
2024-03-13 15:25:14 -0300

    exercicios

---
+++ Exercicios/ex-01-ate-06.py
@@ -0,0 +1,20 @@
+#Ex-01
+num = int(input('Digite um número: '))
+sucessor = num + 1
+antecessor = num - 1
+print(f'O número {num} tem esse antecessor {antecessor} e o sucessor {sucessor}')
+
+#Ex-02
+n1 = int(input('Digite 1 número: '))
+n2 = int(input('Digite 1 número: '))
+n3 = int(input('Digite 1 número: '))
+Sarit = n1 + n2 + n3 / 3
+print(f'A soma Aritmetica dos números é: {Sarit}')
+
+#Ex-03
+ds1 = 0.00
+ds2 = 0.05
+ds3 = 0.10
+ds4 = 0.15
+valor = float(input('Digite um valor'))
+if valor >= 9.99
